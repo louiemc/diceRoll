eee0a8d8faddb9e7bcaae235d208869dedffd4dd
Plant Lady <codeplantsbylouie@gmail.com>
2019-10-07 13:56:59 -0400

    basic variables created

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer;
+
+scores = [0 , 0];
+roundScore = 0;
+activePlayer = 0;
