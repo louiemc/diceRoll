16678fa8e6d1887ec47dc5ad77e6d05c52f835c2
Plant Lady <codeplantsbylouie@gmail.com>
2019-10-07 14:18:51 -0400

    establishing the dice variables, dice images and conditional statements

--- app.js
+++ app.js
@@ -9,8 +9,32 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0 , 0];
-roundScore = 0;
-activePlayer = 0;
+init();
+
+let lastDice;
+
+document.querySelector('.btn-roll').addEventListener("click", () => {
+  if (gamePlaying) {
+    // Random Number
+    let dice1 = Math.floor(Math.random() * 6) +1;
+    let dice2 = Math.floor(Math.random() * 6) +1;
+
+    // Display Result
+    document.getElementById('dice-1').style.display = "block";
+    document.getElementById('dice-2').style.display = "block";
+    document.getElementById('dice-1').src = "dice-" + dice1 + ".png";
+    document.getElementById('dice-2').src = "dice-" + dice2 + ".png";
+
+    // Update the round score IF the rolled number was NOT a 1
+    if (dice1 !== 1 && dice2 !== 1) {
+      // Add score
+      roundScore += dice1 + dice2;
+      document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+      // Next player
+      nextPlayer();
+    }
+  }
+});
